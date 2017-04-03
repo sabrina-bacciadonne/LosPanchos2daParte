b95a374172c1d5f509a09369496319ab988c0fd1
utnso <ezequielvgs@gmail.com>
2017-04-03 16:24:58 -0300

    Se agrega firma para manejo de archivo de config.

---
+++ Consola/archivo_configuracion.c
@@ -0,0 +1,8 @@
+/*
+ * archivo_configuracion.c
+ *
+ *  Created on: 3/4/2017
+ *      Author: utnso
+ */
+
+
---
+++ Consola/archivo_configuracion.h
@@ -0,0 +1,17 @@
+/*
+ * archivo_configuracion.h
+ *
+ *  Created on: 3/4/2017
+ *      Author: utnso
+ */
+
+#ifndef ARCHIVO_CONFIGURACION_H_
+#define ARCHIVO_CONFIGURACION_H_
+
+
+
+#endif /* ARCHIVO_CONFIGURACION_H_ */
+
+void abrir_archivo_config();
+void validar_archivo_config();
+void cerrar_archivo_config();
