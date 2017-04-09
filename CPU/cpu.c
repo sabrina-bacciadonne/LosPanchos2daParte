bca062eeb519db98bdc8b68d69c6754be908432f
sabrina-bacciadonne <sabacciadonne@yahoo.com.ar>
2017-04-09 17:52:19 -0300

    empezando primitivas

--- CPU/cpu.c
+++ CPU/cpu.c
@@ -1,9 +1,60 @@
 //
 // Created by Martin Gauna on 4/2/17.
 //
-
+/*
+ * #!/usr/bin/ansisop
+ begin
+ variables a, b
+ a = 3
+ b = 5
+ a = b + 12
+ end
+ */
 #include "cpu.h"
+#include "primitivas.h"
+#include <parser/parser.h>
+
+AnSISOP_funciones primitivas = {
+		.AnSISOP_definirVariable		= definirVariable,
+		.AnSISOP_obtenerPosicionVariable= obtenerPosicionVariable,
+		.AnSISOP_dereferenciar			= dereferenciar,
+		.AnSISOP_asignar				= asignar,
+		.AnSISOP_obtenerValorCompartida = obtenerValorCompartida,
+		.AnSISOP_asignarValorCompartida = asignarValorCompartida,
+		.AnSISOP_irAlLabel				= irAlLabel,
+		.AnSISOP_llamarConRetorno		= llamarConRetorno,
+		.AnSISOP_retornar				= retornar,
+		//.AnSISOP_imprimir				= imprimir,
+		//.AnSISOP_imprimirTexto			= imprimirTexto,
+		//.AnSISOP_entradaSalida			= entradaSalida,
+		.AnSISOP_finalizar				= finalizar,
+
+};
+AnSISOP_kernel primitivas_kernel = {
+		.AnSISOP_wait					=wait_kernel,
+		.AnSISOP_signal					=signal_kernel,
+		.AnSISOP_reservar				=reservar_kernel,
+		.AnSISOP_liberar				=liberar_kernel,
+		.AnSISOP_abrir					=abrir_kernel,
+		.AnSISOP_borrar					=borrar_kernel,
+		.AnSISOP_cerrar					=cerrar_kernel,
+		.AnSISOP_moverCursor			=moverCursor_kernel,
+		.AnSISOP_escribir				=escribir_kernel,
+		.AnSISOP_leer					=leer_kernel
+};
 
 int main(int argc, char **argv) {
 
+char* sentencia = 'variables a, b';
+
+analizadorLinea(depurarSentencia(sentencia), &primitivas,&primitivas_kernel);
+
+char* depurarSentencia(char* sentencia) {
+
+	int i = strlen(sentencia);
+	while (string_ends_with(sentencia, "\n")) {
+		i--;
+		sentencia = string_substring_until(sentencia, i);
+	}
+	return sentencia;
 }
---
+++ CPU/primitivas.h
@@ -0,0 +1,59 @@
+#ifndef PRIMITIVAS_H_
+#define PRIMITIVAS_H_
+typedef u_int32_t t_puntero;
+	typedef u_int32_t t_size;
+	typedef t_puntero t_puntero_instruccion;
+	typedef t_puntero t_descriptor_archivo;
+
+	typedef char t_nombre_variable;
+	typedef int t_valor_variable;
+
+	typedef t_nombre_variable* t_nombre_semaforo;
+	typedef t_nombre_variable* t_nombre_etiqueta;
+	typedef  t_nombre_variable* t_nombre_compartida;
+	typedef  t_nombre_variable* t_direccion_archivo;
+
+	typedef enum {
+		SIN_ERROR,
+		NO_EXISTE_VARIABLE,
+		NO_EXISTE_FUNCION,
+		NO_EXISTE_ETIQUETA,
+		STACK_OVERFLOW,
+		STACK_UNDERFLOW,
+		FIN_PROGRAMA,
+	} STATUS;
+
+	/*typedef struct{
+		bool lectura;
+		bool escritura;
+		bool creacion;
+	} t_banderas;*/
+
+	t_puntero definirVariable(t_nombre_variable identificador_variable);
+	t_puntero obtenerPosicionVariable (t_nombre_variable identificador_variable);
+	t_valor_variable dereferenciar(t_puntero direccion_variable);
+	void asignar(t_puntero direccion_variable,t_valor_variable valor);
+	t_valor_variable obtenerValorCompartida(t_nombre_compartida variable);
+	t_valor_variable asignarValorCompartida(t_nombre_compartida variable,t_valor_variable valor);
+	void irAlLabel(t_nombre_etiqueta etiqueta);
+	//t_puntero_instruccion llamarFuncion(t_nombre_etiqueta etiqueta,t_puntero donde_retornar,t_puntero_instruccion linea_en_ejecucion);
+	void llamarSinRetorno(t_nombre_etiqueta etiqueta);
+	void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
+	void finalizar(void);
+	void retornar(t_valor_variable retorno);
+	//void imprimir(t_valor_variable valor_mostrar);
+	//void imprimirTexto(char*texto); no estaban en el parser.h
+	//void entradaSalida(t_nombre_dispositivo dispositivo,int tiempo); no reconoce t_nombre_dispositivo
+	void finalizar();
+	void wait_kernel(t_nombre_semaforo identificador_semaforo);
+	void signal_kernel(t_nombre_semaforo identificador_semaforo);
+	t_puntero reservar_kernel(t_valor_variable espacio);
+	void liberar_kernel(t_puntero puntero);
+	t_descriptor_archivo abrir_kernel(t_direccion_archivo direccion, t_banderas flags);
+	void borrar_kernel(t_descriptor_archivo direccion);
+	void cerrar_kernel(t_descriptor_archivo descriptor_archivo);
+	void moverCursor_kernel(t_descriptor_archivo descriptor_archivo, t_valor_variable posicion);
+	void escribir_kernel(t_descriptor_archivo descriptor_archivo, void* informacion, t_valor_variable tamanio);
+	void leer_kernel(t_descriptor_archivo descriptor_archivo, t_puntero informacion, t_valor_variable tamanio);
+
+#endif /* PRIMITIVAS_H_ */
