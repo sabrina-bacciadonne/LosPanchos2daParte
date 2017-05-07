ded19142f21de6ab7931ed13c3c7e6b692f62607
Esteban Paz Reyes <esteban.pazreyes@gmail.com>
2017-05-07 14:05:30 -0300

    Se sube makefile de memoria y se adecua su encabezado

--- Memoria/memoria.h
+++ Memoria/memoria.h
@@ -5,12 +5,12 @@
 #ifndef TP_2017_1C_LOSPANCHOS_MEMORIA_H
 #define TP_2017_1C_LOSPANCHOS_MEMORIA_H
 
-#include "../Commons/configuracion.h"
-#include "../Commons/socket.h"
-#include "../Commons/console.h"
 #include <commons/log.h>
 #include <commons/config.h>
 
+#include <utilstp/console.h>
+#include <utilstp/configuracion.h>
+#include <utilstp/socket.h>
 
 #endif //TP_2017_1C_LOSPANCHOS_MEMORIA_H
 
